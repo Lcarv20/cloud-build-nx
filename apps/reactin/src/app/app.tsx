fc2454a41a69a5735363394bcd51c8beeba524c4
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2022-03-07 01:05:39 +0100

    Adding missing step on the cloudconfig file. Adjusting prettier settings

--- apps/reactin/src/app/app.tsx
+++ apps/reactin/src/app/app.tsx
@@ -3,13 +3,13 @@ import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="reactin" />
-      <h1>I should've added something otherwhise the build doesn't work</h1>
-      <div />
-    </>
-  );
+	return (
+		<>
+			<NxWelcome title="reactin" />
+			<h1>I should've added something otherwhise the build doesn't work</h1>
+			<div />
+		</>
+	);
 }
 
 export default App;
